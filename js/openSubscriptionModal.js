c4e3afde1eb1243781fd5a8babb0d7767c3fdc0a
RodriGarcia12 <rgarciaa125@gmail.com>
2022-10-26 17:39:12 -0300

    boton para abrir modal de subs

--- js/openSubscriptionModal.js
+++ js/openSubscriptionModal.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    $(".aside-right__ad-delete").click(function(){
+    $(".aside-right__ad-container__ad-delete").click(function(){
         $(".section-modal").show(120);
     });
     $(".section-modal__close").click(function(){

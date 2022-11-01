e25fe4569232bfeea0575bba4f659c68bc1c0b56
RodriGarcia12 <rgarciaa125@gmail.com>
2022-10-31 23:53:47 -0300

    abrir modals para register y pagos

---
+++ js/openPayModal.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    $(".section-modal__main-container__card-group__card-container__button").click(function(){
+        $(".section-modal").hide(120);
+        $(".pay-modal").show(120);
+    });
+});
---
+++ js/openRegisterModal.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+    $("#main-login__login-card__register-id").click(function(){
+        $(".register-modal").show(120);
+    });
+});

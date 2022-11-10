08e3167cf044a56ebf52d32ff08ad537d3018a25
RodriGarcia12 <rgarciaa125@gmail.com>
2022-11-10 13:03:08 -0300

    hamburger nav

--- js/hamburguerMenu.js
+++ js/hamburguerMenu.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
-    $(".nav__hamburger-container__main-group").hide();
-    $(".nav__hamburger-container__btn").hide();
-    $(".nav__hamburger-container__cross").hide();
+    $(".nav__hamburger-container__main-group").css("display", "none");
+    $(".nav__hamburger-container__btn").css("display", "none");
+    $(".nav__hamburger-container__cross").css("display", "none");
     if ($(window).width() < 620) {
-        $(".nav__hamburger-container__main-group").hide();
-        $(".nav__hamburger-container__cross").hide();
-        $(".nav__hamburger-container__btn").show();
+        $(".nav__hamburger-container__main-group").css("display", "none");
+        $(".nav__hamburger-container__cross").css("display", "none");
+        $(".nav__hamburger-container__btn").css("display", "inline");
         $(".nav__hamburger-container__btn").click(function(){
             $(".nav__hamburger-container__main-group").slideToggle(1200);
             $(".nav__hamburger-container__btn").hide();
